b0bb3f8155338a0e3f64e032c3fae64fb5122d55
hobbitmagic <151260692+hobbitmagic@users.noreply.github.com>
2023-12-12 13:29:43 -0700

    Adding day 9

---
+++ 2023/9/main2.py
@@ -0,0 +1,43 @@
+"""Challenge 9B"""
+
+def main():
+    """Main"""
+    with open("input.txt", "rt", encoding='UTF-8') as file:
+        total = 0
+        for line in file:
+            line = line.strip()
+            if line == '' or line is None:
+                continue
+
+            numbers = []
+            numbers.append(list(map(int,line.split())))
+
+            print(numbers)
+            line_num = 0
+            while True:
+                all_zeros = True
+                numbers.append([])
+                for index, number in enumerate(numbers[line_num]):
+                    if index == 0:
+                        continue
+                    current = number - numbers[line_num][index-1]
+                    if current != 0:
+                        all_zeros = False
+                    numbers[line_num + 1].append(current)
+                if all_zeros:
+                    break
+
+                line_num += 1
+            print(numbers)
+            i = len(numbers) - 2
+            temp_number = 0
+            while i >= 0:
+                temp_number = numbers[i][0] - temp_number
+                i -= 1
+            total += temp_number
+
+
+    print("ANSWER:", total)
+
+if __name__ == "__main__":
+    main()
